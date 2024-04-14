6c3056ada09e66d017aab196621f2505cfbb6552
annuj-vishwakarma <vishwakarmaanuj61@gmail.com>
2024-04-14 23:35:32 +0530

    initial

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
-import express from "express";
-import dotenv from "dotenv"
-import cors from "cors";
-import {dbconnection} from './Database/dbconnection'
-
+require("dotenv").config({path:'./.env'})
+require("./Database/dbconnection").dbConnection();
+const express = require("express")
 const app = express();
+
 app.listen(
     process.env.PORT,
     console.log(`server running on port ${process.env.PORT}`)
