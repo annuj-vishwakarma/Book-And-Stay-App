49a1375b61c5a74c8a57f617293f10945076db88
annuj-vishwakarma <vishwakarmaanuj61@gmail.com>
2024-04-29 11:11:20 +0530

    database connection

--- server/Database/dbconnection.js
+++ server/Database/dbconnection.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose")
-
-exports.dbConnection=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("database connected successfully");
-     } catch (error) {
-        console.log("error.message")
-     }
-};
-
-
+//database connection code here !!!!
